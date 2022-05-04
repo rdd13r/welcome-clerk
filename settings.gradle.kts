8a5e265b2e37bd4e64992b74e2494d2aa1d57f8a
Vadim Kuhay <12781006+rdd13r@users.noreply.github.com>
2022-05-04 16:12:44 -0400

    welcome-clerk-13 Task: Convert to rich multi-project with 'foreign' inclusions
    Pushed down aggregate component so that domain and peer artifacts can be created.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+    plugins {
+        kotlin("multiplatform").version("1.6.21")
+        id("io.gitlab.arturbosch.detekt").version("1.20.0")
+        id("org.jetbrains.qodana").version("0.1.13")
+    }
+}
+
+
 rootProject.name = "welcome-clerk"
+
+include(":domain:root")
